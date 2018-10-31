a44a042aef0084283ad838af408a682927f7c704
Thomas Lorntzen <cyrthomas1@gmail.com>
2018-11-01 00:17:25 +0100

    Transcript class progress

--- src/fillerWords.js
+++ src/fillerWords.js
@@ -1,68 +1,75 @@
-export default {
-    words: [
-      {
-        word: "liksom",
-        count: 0
-      },
-      {
-        word: "AS",
-        count: 0
-      },
-      {
-        word: "ASA",
-        count: 0
-      },
-      {
-        word: "ass",
-        count: 0
-      },
-      {
-        word: "assa",
-        count: 0
-      },
-      {
-        word: "altså",
-        count: 0
-      },
-      {
-        word: "bare",
-        count: 0
-      },
-      {
-        word: "på en måte",
-        count: 0
-      },
-      {
-        word: "holdt jeg på å si",
-        count: 0
-      },
-      {
-        word: "hadde jeg nær sagt",
-        count: 0
-      },
-      {
-        word: "når det er sagt",
-        count: 0
-      },
-      {
-        word: "egentlig",
-        count: 0
-      },
-      {
-        word: "for å si det sånn",
-        count: 0
-      },
-      {
-        word: "for å si det på den måten",
-        count: 0
-      },      
-      {
-        word: "hvis du kjønner hva jeg mener",
-        count: 0
-      },
-      {
-        word: "ikke sant",
-        count: 0
-      }
-    ]
+let fillerWords = [
+  {
+    word: "liksom",
+    count: 0
+  },
+  {
+    word: "AS",
+    count: 0
+  },
+  {
+    word: "ASA",
+    count: 0
+  },
+  {
+    word: "ass",
+    count: 0
+  },
+  {
+    word: "assa",
+    count: 0
+  },
+  {
+    word: "altså",
+    count: 0
+  },
+  {
+    word: "bare",
+    count: 0
+  },
+  {
+    word: "på en måte",
+    count: 0
+  },
+  {
+    word: "holdt jeg på å si",
+    count: 0
+  },
+  {
+    word: "hadde jeg nær sagt",
+    count: 0
+  },
+  {
+    word: "når det er sagt",
+    count: 0
+  },
+  {
+    word: "egentlig",
+    count: 0
+  },
+  {
+    word: "for å si det sånn",
+    count: 0
+  },
+  {
+    word: "for å si det på den måten",
+    count: 0
+  },      
+  {
+    word: "hvis du kjønner hva jeg mener",
+    count: 0
+  },
+  {
+    word: "ikke sant",
+    count: 0
+  }
+];
+
+class Transcript {
+  constructor(text, fillerWords) {
+    this.text = text;
+    this.fillerWords = fillerWords;
+  }
 }
+
+export { Transcript as default }
