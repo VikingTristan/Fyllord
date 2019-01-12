c6973c1b43813e51b1db3ff4ae26d042a7973fdd
Thomas Lorntzen <cyrthomas1@gmail.com>
2019-01-12 09:40:57 +0100

    Fix Transcript class

--- src/transcript.js
+++ src/transcript.js
@@ -1,74 +1,72 @@
-const fillerWords = [
-  {
-    word: "liksom",
-    count: 0
-  },
-  {
-    word: "AS",
-    count: 0
-  },
-  {
-    word: "ASA",
-    count: 0
-  },
-  {
-    word: "ass",
-    count: 0
-  },
-  {
-    word: "assa",
-    count: 0
-  },
-  {
-    word: "altså",
-    count: 0
-  },
-  {
-    word: "bare",
-    count: 0
-  },
-  {
-    word: "på en måte",
-    count: 0
-  },
-  {
-    word: "holdt jeg på å si",
-    count: 0
-  },
-  {
-    word: "hadde jeg nær sagt",
-    count: 0
-  },
-  {
-    word: "når det er sagt",
-    count: 0
-  },
-  {
-    word: "egentlig",
-    count: 0
-  },
-  {
-    word: "for å si det sånn",
-    count: 0
-  },
-  {
-    word: "for å si det på den måten",
-    count: 0
-  },      
-  {
-    word: "hvis du skjønner hva jeg mener",
-    count: 0
-  },
-  {
-    word: "ikke sant",
-    count: 0
-  }
-];
-
 class Transcript {
   constructor(text) {
     this.text = text || "";
-    this.fillerWords = [...fillerWords];
+    this.fillerWords = [
+      {
+        word: "liksom",
+        count: 0
+      },
+      {
+        word: "AS",
+        count: 0
+      },
+      {
+        word: "ASA",
+        count: 0
+      },
+      {
+        word: "ass",
+        count: 0
+      },
+      {
+        word: "assa",
+        count: 0
+      },
+      {
+        word: "altså",
+        count: 0
+      },
+      {
+        word: "bare",
+        count: 0
+      },
+      {
+        word: "på en måte",
+        count: 0
+      },
+      {
+        word: "holdt jeg på å si",
+        count: 0
+      },
+      {
+        word: "hadde jeg nær sagt",
+        count: 0
+      },
+      {
+        word: "når det er sagt",
+        count: 0
+      },
+      {
+        word: "egentlig",
+        count: 0
+      },
+      {
+        word: "for å si det sånn",
+        count: 0
+      },
+      {
+        word: "for å si det på den måten",
+        count: 0
+      },      
+      {
+        word: "hvis du skjønner hva jeg mener",
+        count: 0
+      },
+      {
+        word: "ikke sant",
+        count: 0
+      }
+    ];
   }
 
   reset(){
